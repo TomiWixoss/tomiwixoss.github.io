0e4ad043a329ba1e061d6a7ce10810767f5e69c5
TomiWixoss <kotoricktv@gmail.com>
2024-07-27 09:48:34 +0700

    Thêm giao diện đấu sơ lượt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+'use client'
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const TomiWixoss: React.FC = () => {
+  const router = useRouter();
+
+  const handlePlayClick = () => {
+    router.push('/wixoss'); // Thay đổi đường dẫn đến trang bạn muốn điều hướng
+  };
+
   return (
-    <div className='bg-black'>
-      <div className="flex flex-col items-center justify-center min-h-screen p-4">
+    <div className='bg-black min-h-screen flex items-center justify-center'>
+      <div className="text-center p-4">
         <h1 className="text-4xl font-bold text-white mb-4">TomiWixoss</h1>
         <p className="text-lg text-gray-400 mb-6">Phiên bản PreView0.1</p>
-        <button className="px-6 py-2 text-white rounded-lg bg-blue-500 rounded hover:bg-blue-600 focus:outline-none">
+        <button
+          onClick={handlePlayClick}
+          className="px-6 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+        >
           Chơi Thử!
         </button>
       </div>
---
+++ src/app/wixoss/page.tsx
@@ -0,0 +1,184 @@
+import React from 'react';
+import Image from 'next/image';
+
+const PlayGround: React.FC = () => {
+    return (
+        <div className="bg-black h-screen py-1 px-2">
+            <div className='flex justify-center items-center'>
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[15%] h-auto'
+                />
+                <p className='text-2xl text-white mx-4'>x1</p>
+                <div className='flex flex-col justify-center items-center'>
+                    <button
+                        className="px-2 py-1 text-xs text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Bộ Bài Chính
+                    </button>
+                    <button
+                        className="px-2 py-1 text-xs mt-1 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Bộ Bài LRIG
+                    </button>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <button
+                        className="px-2 py-1 text-xs text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Thùng Rác
+                    </button>
+                    <button
+                        className="px-2 py-1 text-xs mt-1 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Nguyên Liệu
+                    </button>
+                </div>
+            </div>
+            <div className='flex justify-center mt-5 items-center'>
+                <Image
+                    src={'/backside/LRIG.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+                <Image
+                    src={'/backside/LRIG.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] mx-12 h-auto'
+                />
+                <Image
+                    src={'/backside/LRIG.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+            </div>
+            <div className='flex justify-center mt-5 items-center'>
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] mx-12 h-auto'
+                />
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+            </div>
+            <div className='flex justify-center items-center'>
+                <button
+                    className="px-2 py-1 text-xs text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                >
+                    Tay Đối Thủ
+                </button>
+                <p className='text-white my-5 text-center mx-5'>Lượt 1</p>
+                <button
+                    className="px-2 py-1 text-xs text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                >
+                    Tay Người Chơi
+                </button>
+            </div>
+            <div className='flex justify-center items-center'>
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] mx-12 h-auto'
+                />
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+            </div>
+            <div className='flex justify-center mt-5 items-center'>
+                <Image
+                    src={'/backside/LRIG.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+                <Image
+                    src={'/backside/LRIG.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] mx-12 h-auto'
+                />
+                <Image
+                    src={'/backside/LRIG.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[20%] h-auto'
+                />
+            </div>
+            <div className='flex justify-center mt-5 items-center'>
+                <Image
+                    src={'/backside/MAIN.jpg'}
+                    alt={'Ảnh bìa chính'}
+                    width={750}
+                    height={1047}
+                    className='w-[15%] h-auto'
+                />
+                <p className='text-2xl text-white mx-4'>x1</p>
+                <div className='flex flex-col justify-center items-center'>
+                    <button
+                        className="px-2 py-1 text-xs text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Bộ Bài Chính
+                    </button>
+                    <button
+                        className="px-2 py-1 text-xs mt-1 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Bộ Bài LRIG
+                    </button>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <button
+                        className="px-2 py-1 text-xs text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Thùng Rác
+                    </button>
+                    <button
+                        className="px-2 py-1 text-xs mt-1 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+                    >
+                        Nguyên Liệu
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PlayGround;
