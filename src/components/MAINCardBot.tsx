84e07492be7563c8c67f98eefebdc09fc06a37ab
TomiSakae <tomisakaeanime@gmail.com>
2024-08-04 23:03:32 +0700

    Thêm khả năng xem và chọn bài trong bộ bài chính

--- src/components/MAINCardBot.tsx
+++ src/components/MAINCardBot.tsx
@@ -4,6 +4,8 @@ import { IoMdClose } from "react-icons/io";
 import CardDetail from './CardDetail';
 import Card from "../types/cardList";
 import cardList from './CardDB';
+import { FaCheck } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa";
 
 interface MAINPopupProps {
     isOpen: boolean;
@@ -24,6 +26,10 @@ const MAINPopup: React.FC<MAINPopupProps> = ({ isOpen, onClose, numberCardMAIN, 
     const [selectedCard, setSelectedCard] = useState<Card | null>(null);
     const [isPopupAction, setPopupAction] = useState(false);
     const [isPopupRefresh, setPopupRefresh] = useState(false);
+    const [currentCardIndex, setCurrentCardIndex] = useState<number>(0);
+    const [flippedCards, setFlippedCards] = useState<Set<number>>(new Set());
+    const [temporarySelectedCards, setTemporarySelectedCards] = useState<number[]>([]);
+    const [flippedCardList, setFlippedCardList] = useState<number[]>([]);
 
     useEffect(() => {
         if (isOpen === true) {
@@ -47,6 +53,30 @@ const MAINPopup: React.FC<MAINPopupProps> = ({ isOpen, onClose, numberCardMAIN, 
         setPopupAction(true);
     };
 
+    const handleCardClickChoose = (index: number) => {
+        setTemporarySelectedCards(prev => {
+            if (prev.includes(index)) {
+                // Nếu thẻ đã được chọn, bỏ chọn
+                return prev.filter(item => item !== index);
+            } else {
+                // Nếu thẻ chưa được chọn, chọn
+                return [...prev, index];
+            }
+        });
+    };
+
+    const handleCardFlip = () => {
+        setFlippedCards(prev => {
+            const updatedFlippedCards = new Set(prev);
+            updatedFlippedCards.add(currentCardIndex);
+            return updatedFlippedCards;
+        });
+
+        setFlippedCardList(prev => [...prev, currentCardIndex]);
+
+        setCurrentCardIndex(prev => prev + 1);
+    };
+
     const handleCloseDetail = () => {
         setSelectedCard(null);
     };
@@ -57,26 +87,91 @@ const MAINPopup: React.FC<MAINPopupProps> = ({ isOpen, onClose, numberCardMAIN, 
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
                     <div className="bg-white p-5 m-5 rounded-lg shadow-lg text-center relative overflow-auto max-h-[80vh]">
                         <div className="flex justify-between items-center mb-4">
-                            <p className="font-bold text-lg">Bộ Bài Chính ({numberCardMAIN.length})</p>
-                            <IoMdClose
-                                onClick={onClose}
-                                className="font-bold text-2xl cursor-pointer"
-                            />
+                            <div className='flex justify-center items-center'>
+                                <p className="font-bold text-lg">
+                                    Bộ Bài Chính ({numberCardMAIN.length})
+                                </p>
+                                <FaEye
+                                    className="font-bold text-2xl cursor-pointer ml-2"
+                                    onClick={handleCardFlip}
+                                />
+                            </div>
+                            <div className='flex justify-center items-center'>
+                                {temporarySelectedCards.length > 0 &&
+                                    <FaCheck className="font-bold text-2xl cursor-pointer mr-2"
+                                        onClick={() => {
+                                            // Sao chép danh sách thẻ
+                                            const cardHand = [...numberCardHand];
+                                            const cardMAIN = [...numberCardMAIN];
+
+                                            // Thêm các thẻ được chọn vào cardHand và loại bỏ khỏi cardMAIN
+                                            temporarySelectedCards.forEach(index => {
+                                                const cardPut = cardMAIN[index];
+                                                if (cardPut) cardHand.push(cardPut);
+                                            });
+
+                                            // Xác định các thẻ trong flippedCardList không nằm trong temporarySelectedCards
+                                            const remainingFlippedCards = flippedCardList.filter(cardIndex =>
+                                                !temporarySelectedCards.includes(cardIndex)
+                                            );
+
+                                            const cardPutMAIN: number[] = [];
+                                            remainingFlippedCards.forEach(index => {
+                                                const cardPut = cardMAIN[index];
+                                                if (cardPut) cardPutMAIN.push(cardPut);
+                                            });
+
+                                            // Xáo trộn danh sách cardMAIN
+                                            const shuffledCardMAIN = shuffleArray(cardPutMAIN);
+
+                                            // Thêm các thẻ không nằm trong temporarySelectedCards vào cardMAIN ở cuối danh sách
+                                            const updatedCardMAIN = [...cardMAIN, ...shuffledCardMAIN];
+
+                                            // Cập nhật trạng thái
+                                            setNumberCardHand(cardHand);
+                                            setNumberCardMAIN(updatedCardMAIN.splice(flippedCardList.length));
+                                            setTemporarySelectedCards([]);
+                                            setFlippedCards(new Set());
+                                            setCurrentCardIndex(0);
+                                            setFlippedCardList([]);
+                                            onClose();
+                                        }}
+                                    />
+                                }
+                                <IoMdClose
+                                    onClick={() => {
+                                        setFlippedCards(new Set());
+                                        setCurrentCardIndex(0);
+                                        setTemporarySelectedCards([]);
+                                        setFlippedCardList([]);
+                                        onClose();
+                                    }}
+                                    className="font-bold text-2xl cursor-pointer"
+                                />
+                            </div>
                         </div>
                         <div className="mt-4 grid grid-cols-3 gap-4 max-h-[60vh] overflow-y-auto">
-                            {numberCardMAIN.map((index) => {
-                                // Sinh ra giá trị ngẫu nhiên, ví dụ từ 1 đến ...
-                                const randomValue = Math.floor(Math.random() * 999999999) + 1;
+                            {numberCardMAIN.map((index, i) => {
+                                const isFlipped = flippedCards.has(i);
+                                const isSelected = temporarySelectedCards.includes(i);
                                 return (
-                                    <div key={`${index}-${randomValue}`} className="flex flex-col items-center cursor-pointer">
+                                    <div key={`${index}-${i}`} className={`flex flex-col items-center cursor-pointer`}>
                                         <Image
-                                            src={'/backside/MAIN.jpg'}
-                                            alt={'backside'}
+                                            src={isFlipped ? (cardList.find(card => card.id === index)?.imageUrl ?? '/backside/MAIN.jpg') : '/backside/MAIN.jpg'}
+                                            alt={'card'}
                                             width={750}
                                             height={1047}
-                                            className="w-full h-auto mb-2"
+                                            className={`w-full h-auto mb-2 ${temporarySelectedCards.includes(i) ? 'border-[3px] rounded-md border-red-500' : ''}`}
                                             onClick={handleCardClick}
                                         />
+                                        {isFlipped && (
+                                            <button
+                                                className="px-2 py-1 bg-blue-500 text-white text-xs rounded-xl hover:bg-blue-600"
+                                                onClick={() => handleCardClickChoose(i)}
+                                            >
+                                                {isSelected ? 'Bỏ Chọn' : 'Chọn'}
+                                            </button>
+                                        )}
                                     </div>
                                 );
                             })}
@@ -206,7 +301,7 @@ const MAINPopup: React.FC<MAINPopupProps> = ({ isOpen, onClose, numberCardMAIN, 
                             </button>
                         </div>
                     </div>
-                </div >
+                </div>
             )}
             {selectedCard && <CardDetail card={selectedCard} onClose={handleCloseDetail} />}
         </>
